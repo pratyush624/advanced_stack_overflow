3ecb4cfee09c3dfa3c6055f8fab8f1d921d844b6
Pratyush <pratyush624@gmail.com>
2023-11-29 12:33:08 +0530

    auth_layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Stack Overflow",
+  description:
+    "A community-driven platform where developers can ask and answer programming-related questions",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
